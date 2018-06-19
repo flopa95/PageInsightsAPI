381cd16fb22768d5da6e0e019ae9c8513393b8e1
Flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-06-19 13:12:54 +0800

    Starting to include Google API

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import requests
+import json
+#------------------------------------------------------------------------------------#
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
+@app.route("/login/<string:name>",methods = ['POST', 'GET'])
+def hello_world(name):
+    #x = name+"x"
+    testurl = "https://cdnetworks.com"
+    request_url = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=" + testurl + "&strategy=mobile&key=AIzaSyAsuAfkLwf8_qnzEMET8bH2BNLNhhntat4"
+
+    url = requests.get(request_url)
+    jsons = json.loads(url.text)
+    #return jsons["title"]
+    return render_template ("result.html", name=name, title=jsons["title"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route("/login",methods = ['POST', 'GET'])
-def hello_world():
-   return "hi"
+def error_message():
+    return "Please enter a domain that you would like to test!"
+
 
 if __name__ == "__main__":
     app.run()
