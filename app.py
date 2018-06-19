f114642f0dc09d98b2da113d1624fb53d5ad37b6
Flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-06-19 18:09:12 +0800

    some updates

--- app.py
+++ app.py
@@ -12,12 +12,17 @@ def main():
 def hello_world(name):
     #x = name+"x"
     testurl = "https://cdnetworks.com"
-    request_url = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=" + testurl + "&strategy=mobile&key=AIzaSyAsuAfkLwf8_qnzEMET8bH2BNLNhhntat4"
+    request_url = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=" + testurl + "&strategy=mobile&key=AIzaSyDplKio3HHteEPFPN-fkDquFeHKVodlJBw"
 
     url = requests.get(request_url)
     jsons = json.loads(url.text)
     #return jsons["title"]
-    return render_template ("result.html", name=name, title=jsons["title"])
+    return render_template ("result.html",
+    name=name,
+    title=jsons["title"],
+    speed_score=str(jsons["ruleGroups"]["SPEED"]["score"]) + "/100",
+    loading_dist=jsons["loadingExperience"]["metrics"]["FIRST_CONTENTFUL_PAINT_MS"]["median"],
+    img_opt=jsons["formattedResults"]["ruleResults"]["OptimizeImages"]["urlBlocks"]["header"]["value"])
 
 
 
