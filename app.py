f2d24731e1083c52367558e648666ddda9ead29f
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-06-23 11:11:29 +0800

    Now with passing URL

--- app.py
+++ app.py
@@ -8,44 +8,24 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route("/login/<string:name>",methods = ['POST', 'GET'])
-def hello_world(name):
-    #x = name+"x"
-    testurl = "https://cdnetworks.com"
+@app.route("/testresults",methods = ['POST'])
+def hello_world():
+
+    #testurl = "https://cdnetworks.com"
+    testurl = request.form['text']
     request_url = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=" + testurl + "&strategy=mobile&key=AIzaSyDplKio3HHteEPFPN-fkDquFeHKVodlJBw"
 
     url = requests.get(request_url)
     jsons = json.loads(url.text)
-    #return jsons["title"]
+
     return render_template ("result.html",
-    name=name,
+    name=testurl,
     title=jsons["title"],
     speed_score=str(jsons["ruleGroups"]["SPEED"]["score"]) + "/100",
     loading_dist=jsons["loadingExperience"]["metrics"]["FIRST_CONTENTFUL_PAINT_MS"]["median"],
-<<<<<<< HEAD
     file_size_savings=jsons["formattedResults"]["ruleResults"]["OptimizeImages"]["urlBlocks"][0]["header"]["args"][1]["value"],
     img_opt=jsons["formattedResults"]["ruleResults"]["OptimizeImages"]["urlBlocks"][0]["header"]["args"][2]["value"])
-=======
-    img_opt=jsons["formattedResults"]["ruleResults"]["OptimizeImages"]["urlBlocks"]["header"]["value"])
->>>>>>> origin/master
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route("/login",methods = ['POST', 'GET'])
-def error_message():
-    return "Please enter a domain that you would like to test!"
+    
 
 
 if __name__ == "__main__":
