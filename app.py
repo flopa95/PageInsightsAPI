a9e182d460222ce5662ae881ea1a8096b7b5f2f4
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-07-02 19:42:48 +0800

    Including KeyErrors

--- app.py
+++ app.py
@@ -13,18 +13,38 @@ def hello_world():
 
     #testurl = "https://cdnetworks.com"
     testurl = request.form['text']
-    request_url = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=" + testurl + "&strategy=mobile&key=AIzaSyDplKio3HHteEPFPN-fkDquFeHKVodlJBw"
+    request_url_mobile = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=" + testurl + "&strategy=mobile&key=AIzaSyDplKio3HHteEPFPN-fkDquFeHKVodlJBw"
+    request_url_desktop = "https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=" + testurl + "&strategy=desktop&key=AIzaSyDplKio3HHteEPFPN-fkDquFeHKVodlJBw"
 
-    url = requests.get(request_url)
-    jsons = json.loads(url.text)
+    url = requests.get(request_url_mobile)
+    url2 = requests.get(request_url_desktop)
+    
+    mobiledata = json.loads(url.text)
+    desktopdata = json.loads(url2.text)
+
+#Checking the main details
+    try:
+        title = mobiledata["title"]
+        mobile_speed_score = str(mobiledata["ruleGroups"]["SPEED"]["score"]) + "/100"
+        loading_dist = mobiledata["loadingExperience"]["metrics"]["FIRST_CONTENTFUL_PAINT_MS"]["median"]
+        
+    except KeyError:
+        title = "N/A"
+        mobile_speed_score = "N/A"
+        loading_dist = "N/A"
+
+#Checking specific for image optimization      
+    try:
+        file_size_savings = "You can reduce your website's images by " + mobiledata["formattedResults"]["ruleResults"]["OptimizeImages"]["urlBlocks"][0]["header"]["args"][1]["value"]
+        img_opt = "(" + mobiledata["formattedResults"]["ruleResults"]["OptimizeImages"]["urlBlocks"][0]["header"]["args"][2]["value"] + " reduction)."
+    except KeyError:
+         file_size_savings = "Your images are fully optimized."
+         img_opt = ""
+    
+
 
     return render_template ("result.html",
-    name=testurl,
-    title=jsons["title"],
-    speed_score=str(jsons["ruleGroups"]["SPEED"]["score"]) + "/100",
-    loading_dist=jsons["loadingExperience"]["metrics"]["FIRST_CONTENTFUL_PAINT_MS"]["median"],
-    file_size_savings=jsons["formattedResults"]["ruleResults"]["OptimizeImages"]["urlBlocks"][0]["header"]["args"][1]["value"],
-    img_opt=jsons["formattedResults"]["ruleResults"]["OptimizeImages"]["urlBlocks"][0]["header"]["args"][2]["value"])
+    name=testurl, title=title, mobile_speed_score=mobile_speed_score, loading_dist=loading_dist, file_size_savings=file_size_savings, img_opt=img_opt)
     
 
 
